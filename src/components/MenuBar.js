4759a356ca9668c365d5a5d213ba23ca3da9eec3
saidugurlu <ugurlumsaid@gmx.de>
2022-06-08 13:31:33 +0200

    edit size menübar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { useState } from "react";
+import { Menu } from "semantic-ui-react";
 
 const MenuBar = () => {
- const [activeItem, setActiveItem] = useState('home')
+  const [activeItem, setActiveItem] = useState("home");
 
- const handleItemClick = (e, { name })=> {  setActiveItem({ activeItem: name })}
+  const handleItemClick = (e, { name }) => {
+    setActiveItem({ activeItem: name });
+  };
 
   return (
+    <div>
+      <Menu size='massive' pointing>
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="messages"
+          active={activeItem === "messages"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="friends"
+          active={activeItem === "friends"}
+          onClick={handleItemClick}
+        />
+      </Menu>
+    </div>
+  );
+};
 
-    
-    <div><Menu pointing>
-    <Menu.Item
-      name='home'
-      active={activeItem === 'home'}
-      onClick={handleItemClick}
-    />
-    <Menu.Item
-      name='messages'
-      active={activeItem === 'messages'}
-      onClick={handleItemClick}
-    />
-    <Menu.Item
-      name='friends'
-      active={activeItem === 'friends'}
-      onClick={handleItemClick}
-    />
- 
-  </Menu> </div>
-  )
-}
-
-export default MenuBar
+export default MenuBar;

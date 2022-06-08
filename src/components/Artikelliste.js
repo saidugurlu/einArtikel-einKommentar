878badb2b759e5123db2853f5ba8bc0e9fe3f25c
saidugurlu <ugurlumsaid@gmx.de>
2022-06-08 17:17:53 +0200

    loading added

--- src/components/Artikelliste.js
+++ src/components/Artikelliste.js
@@ -7,23 +7,39 @@ const Artikelliste = () => {
 
   return (
     <>
-      {articles.map((article, index) => {
-        return (
-          <div key={index} className="ui relaxed divided list">
-            <div className="item">
-              <i className="large quote left middle aligned icon"></i>
-              <div className="content">
-                <Link to={`/posts/${article.id}`} className="header">
-                  {article.title}
-                </Link>
-                <div className="description">
-                  {dateFormatter(article.created_at)}
-                </div>
-              </div>
+    {articles.length === 0 
+    
+    ? (<div class="ui">
+    <div class="content segment">
+      <div class="ui  placeholder">
+        <div class="square image"><div class="ui active dimmer">
+    <div class="ui large text loader">Loading</div>
+  </div>
+  <p></p></div>
+      </div>
+    </div>
+  </div>)
+  : (  articles.map((article, index) => {
+    return (
+      <div key={index} className="ui relaxed divided list">
+        <div className="item">
+          <i className="large quote left middle aligned icon"></i>
+          <div className="content">
+            <Link to={`/posts/${article.id}`} className="header">
+              {article.title}
+            </Link>
+            <div className="description">
+              {dateFormatter(article.created_at)}
             </div>
           </div>
-        );
-      })}
+        </div>
+      </div>
+    );
+  }))
+
+
+    }
+    
     </>
   );
 };

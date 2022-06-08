2cd07a9cc4bd3095601f2f0470ba908c6f978f00
saidugurlu <ugurlumsaid@gmx.de>
2022-06-08 17:44:55 +0200

    react-fade-in added and work

--- src/components/Artikelliste.js
+++ src/components/Artikelliste.js
@@ -1,45 +1,42 @@
 import { Link } from "react-router-dom";
 import { AppContext } from "../AppContext";
 import { useContext } from "react";
+import FadeIn from "react-fade-in";
 
 const Artikelliste = () => {
   const { articles, dateFormatter } = useContext(AppContext);
 
   return (
     <>
-    {articles.length === 0 
-    
-    ? (<div class="ui">
-    <div class="content segment">
-      <div class="ui  placeholder">
-        <div class="square image"><div class="ui active dimmer">
-    <div class="ui large text loader">Loading</div>
-  </div>
-  <p></p></div>
-      </div>
-    </div>
-  </div>)
-  : (  articles.map((article, index) => {
-    return (
-      <div key={index} className="ui relaxed divided list">
-        <div className="item">
-          <i className="large quote left middle aligned icon"></i>
-          <div className="content">
-            <Link to={`/posts/${article.id}`} className="header">
-              {article.title}
-            </Link>
-            <div className="description">
-              {dateFormatter(article.created_at)}
+      {articles.length === 0 ? (
+        <div class="ui placeholder">
+          <div class="square content image">
+            <div class="ui active dimmer">
+              <div class="ui large text loader">Loading</div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }))
-
-
-    }
-    
+      ) : (
+        articles.map((article, index) => {
+          return (
+            <FadeIn key={index}>
+              <div className="ui relaxed divided list">
+                <div className="item">
+                  <i className="large quote left middle aligned icon"></i>
+                  <div className="content">
+                    <Link to={`/posts/${article.id}`} className="header">
+                      {article.title}
+                    </Link>
+                    <div className="description">
+                      {dateFormatter(article.created_at)}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </FadeIn>
+          );
+        })
+      )}
     </>
   );
 };

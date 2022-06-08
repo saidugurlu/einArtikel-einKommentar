11de0944197a7a1a3eb3b241439a01bc6fe3ad8a
saidugurlu <ugurlumsaid@gmx.de>
2022-06-08 15:07:19 +0200

    edit menubar component render

--- src/components/Artikelliste.js
+++ src/components/Artikelliste.js
@@ -5,14 +5,7 @@ import moment from 'moment';
 
 const Artikelliste = () => {
   const { articles } = useContext(AppContext);
-  moment().subtract(10, 'days').calendar(); // 03/09/2019
-  moment().subtract(6, 'days').calendar();  // Last Wednesday at 1:35 PM
-  moment().subtract(3, 'days').calendar();  // Last Saturday at 1:35 PM
-  moment().subtract(1, 'days').calendar();  // Yesterday at 1:35 PM
-  moment().calendar();                      // Today at 1:35 PM
-  moment().add(1, 'days').calendar();       // Tomorrow at 1:35 PM
-  moment().add(3, 'days').calendar();       // Friday at 1:35 PM
-  moment().add(10, 'days').calendar();      // 03/29/2019
+ 
                                            
   
   return (
@@ -27,7 +20,7 @@ const Artikelliste = () => {
                 <Link to={`/posts/${article.id}`} className="header">
                   {article.title}
                 </Link>
-                <div className="description">{moment(article.created_at.toDate()).calendar()}</div>
+                <div className="description">{moment(article.created_at).locale("de").calendar()}</div>
               </div>
             </div>
           </div>
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
 
 const MenuBar = () => {
+  const { getArticles } = useContext(AppContext);
   return (
     <div className="ui inverted segment">
       <div className="ui inverted secondary pointing menu">
-        <NavLink className="item" to="/">
+        <NavLink className="item" to="/" onClick={getArticles()}>
           Home
         </NavLink>
         <NavLink className="item" to="/artikeladd">
